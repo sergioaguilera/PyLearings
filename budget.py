a42e335c840639a745e26c3e0652e8d66aeca68b
Sergio Aguilera <sergio.aaguilera@gmail.com>
2023-10-24 12:00:17 -0600

    Update budget.py
    A program to calculate the average form your budget on 3 months

---
+++ budget.py
@@ -0,0 +1,9 @@
+buget_jan= input("What is your budget on january?")
+buget_feb= input("What is your budget on febrary?")
+buget_mar= input("What is your budget on march?")
+total_budget= int(buget_jan)+int(buget_feb)+int(buget_mar)
+
+print ("Your total budget are ", total_budget)
+
+average_budget=int(total_budget)/3
+print("Your average budget are", average_budget)
