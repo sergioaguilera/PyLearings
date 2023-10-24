466e3a82dae6669364284eea376e7f5d082d83bc
Sergio Aguilera <sergio.aaguilera@gmail.com>
2023-10-24 13:43:53 -0600

    Create odd.py
    Just to see if a number is pair or not

---
+++ odd.py
@@ -0,0 +1,7 @@
+number=input("please add a number, i will tell you if its odd or not:")
+result=int(number)%2
+
+if result != 0:
+    print("The number is odd ")
+else:
+    print("The number is pair")
